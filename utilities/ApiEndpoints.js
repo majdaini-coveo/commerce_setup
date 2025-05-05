39b018e8fcf4828883970c19556d1eac22dc1a19
majdaini <majdaini@coveo.com>
2025-05-05 16:21:30 -0400

    Creating a file with all the API endpoints

---
+++ utilities/ApiEndpoints.js
@@ -0,0 +1,4 @@
+export const CREATE_ORG_ENDPOINT = "/organizations"
+export const CONDITIONS_ENDPOINT = "/search/v1/admin/pipelines/statements"
+export const SHOW_ORGANIZATION_ENDPOINT = "/organizations/"
+export const CREATE_PIPELINES_ENDPOINT = "/search/v1/admin/pipelines/"
