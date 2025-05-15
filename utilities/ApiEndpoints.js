22d115eb5b100deadf2643cddd9744b2fe1d6dac
majdaini <majdaini@coveo.com>
2025-05-15 12:14:54 -0400

    Adding trackingID endpoint

--- utilities/ApiEndpoints.js
+++ utilities/ApiEndpoints.js
@@ -1,4 +1,5 @@
-export const CREATE_ORG_ENDPOINT = "/organizations"
+export const ORGANIZATIONS_ENDPOINT = "/organizations"
 export const CONDITIONS_ENDPOINT = "/search/v1/admin/pipelines/statements"
 export const SHOW_ORGANIZATION_ENDPOINT = "/organizations/"
 export const CREATE_PIPELINES_ENDPOINT = "/search/v1/admin/pipelines/"
+export const TRACKING_ID_ENDPOINT = "/analyticsadmin/v1/properties/"
